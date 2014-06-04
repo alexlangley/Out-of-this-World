49b168c436ea2ed094ec796ff480107f54362373
alexlangley <msg2alex@gmail.com>
2014-06-04 13:56:59 -0500

    Initial Commit

---
+++ Out of this World/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  Out of this World
+//
+//  Created by Alex Langley on 6/4/14.
+//  Copyright (c) 2014 Truu Bruu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Out of this World
+//
+//  Created by Alex Langley on 6/4/14.
+//  Copyright (c) 2014 Truu Bruu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OWAppDelegate class]));
+    }
+}
