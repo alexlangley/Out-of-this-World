12e78fb70b0659b594caf3c6ac47c2c8c3393f5a
alexlangley <msg2alex@gmail.com>
2014-06-04 16:30:57 -0500

    Initial commit, based on lectures

---
+++ Out of this World/OWOuterSpaceTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWOuterSpaceTableViewController.h
+//  Out of this World
+//
+//  Created by Alex Langley on 6/4/14.
+//  Copyright (c) 2014 Truu Bruu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWOuterSpaceTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *planets;
+
+@end
